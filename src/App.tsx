0d7654c0f8cb2a75371845457e28b540676a4af0
Carlos Barragan <carlosbarraganr@gmail.com>
2021-07-06 21:22:22 -0500

    ft/added tailwind css POC

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
+import './tailwind.css';
 
 function App() {
   return (
-    <div className="App" data-testid="AppComponent">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          data-testid="AppLink"
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="bg-gray-700 min-h-screen min-w-full text-white text-3xl flex flex-col justify-center items-center border-2"
+      data-testid="AppComponent"
+    >
+      <img src={logo} className="h-60 animate-spin" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        data-testid="AppLink"
+        className="text-blue-500 hover:underline"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
