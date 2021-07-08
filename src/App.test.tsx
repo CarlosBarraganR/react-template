0316dee2a250f754f82641fc76ad6e85250bd312
Carlos Barragan <carlosbarraganr@gmail.com>
2021-07-07 22:29:47 -0500

    Merge pull request #2 from CarlosBarraganR/ft/styled-components-integration
    ft/styled components integration

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react';
+import { App } from './App';
+import { theme } from './styles/defaultTheme';
 
 test('renders learn react link', () => {
   const { getByTestId } = render(<App />);
   expect(getByTestId('AppComponent')).toBeInTheDocument();
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = getByTestId('AppLink');
   expect(linkElement).toBeInTheDocument();
+  // Assert by text
+  expect(linkElement.textContent).toBe('Learn React');
+  // Assert by style rule
+  expect(linkElement).toHaveStyleRule('color', theme.colors.link);
 });
