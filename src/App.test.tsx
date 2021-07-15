4f24f8176b4aedacbf79243a304cf2b314938e89
Carlos Barragan <carlosbarraganr@gmail.com>
2021-07-14 23:38:55 -0500

    ft/scaffolding-pages added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,9 @@
 import { render } from '@testing-library/react';
 import { App } from './App';
-import { theme } from './styles/defaultTheme';
 
-test('renders learn react link', () => {
-  const { getByTestId } = render(<App />);
-  expect(getByTestId('AppComponent')).toBeInTheDocument();
-  const linkElement = getByTestId('AppLink');
-  expect(linkElement).toBeInTheDocument();
-  // Assert by text
-  expect(linkElement.textContent).toBe('Learn React');
-  // Assert by style rule
-  expect(linkElement).toHaveStyleRule('color', theme.colors.link);
+describe('App', () => {
+  test('renders', () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId('HomePage')).toBeInTheDocument();
+  });
 });
